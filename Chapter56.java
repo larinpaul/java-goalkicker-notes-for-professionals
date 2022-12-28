9b8c40db12e4985046cc74b3a92fa92ce66ff784
Pavel Larin <larinpavelfirst@mail.ru>
2022-12-28 10:39:47 +0500

    Chapter 56 from my Laptop

--- Chapter56.java
+++ Chapter56.java
@@ -92,4 +92,95 @@ char[] password = System.console().readPassword();
 /// 23:25
 
 
+/// 2022 12 28 /// 10:19 ///
 //// Section 56.2: Aligning strings in console
+
+// The method PrintWriter.format (called through System.out.format) can be used to print aligned strings in
+// the console. The method receives a String with the format information and a series of objects to format:
+String rowsStrings[] = new String[] {
+        "1",
+        "1234",
+        "1234567",
+        "123456789"};
+
+String column1Format = "%-3s"; // min 3 characters, left aligned
+String column2Format = "-5.8s"; // min 5 and max 8 characters, left aligned
+String column3Format = "6.6s"; // fixed size 6 characters, right aligned
+String formatInfo = column1Format + " " + column2Format + " " + column3Format;
+
+for (int i = 0; i < rowsStrings.length; i++) {
+    System.out.format(formatInfo, rowsStrings[i], rowsStrings[i], rowsStrings[i]);
+    System.out.println();
+}
+
+// 1    1            1
+// 123 1234       1234
+// 123 1234567  123456
+// 123 12345678 123456
+
+// Format strings examples
+// * %s: just a string with no formatting
+// * %5s: format the string with a minimum of 5 characters; if the string is shorter, it will be padded to 5
+// characters and right aligned
+// * %-5s: format the string with a minimum of 5 charaters; if the string is shorter it will be padded to 5
+// characters and left aligned
+// * %5.10s: format the string with a minimum of 5 characters and a maximum of 10 characters; if the string is
+// shorter than 5 it will be added to 5 characters and right aligned; if the string is longer than 10 it will be
+// truncated to 10 characters and right aligned
+// * %-5.5: fornat the string with a fixed size of 5 characters (minimum and maximum are equals); if the string
+// is shorter than 5 it will be padded to 5 characters and left aligned; if teh string is longer than 5 it will be
+// truncated to 5 characters and left aligned
+
+
+//// Section 56.3: Implementing Basic Command-Line Behavior
+
+// For basic prototypes of basic command-line behavior, the following loop comes in hando.
+public class ExampleCli {
+
+    private static final String CLI_LINE = "example-cli>"; // console linke string
+
+    private static final String CMD_QUIT = "quit"; // string for exiting the program
+    private static final String CMD_HELLO = "hello"; // string for printing "Hello World!" on the screen
+    private static final String CMD_ANSWER = "answer"; // string for printing 42 on the screen
+    public static void main(String[] args) {
+        ExampleCli claimCli = new ExampleCli(); // creates an object of this class
+
+        try {
+            claimCli.start(); // calls the start function to do the work like console
+        }
+        catch (IOException e) {
+            e.printStackTrace(); // prints the exception log if it is failed to do get the user input
+            // or something like that
+        }
+    }
+
+    private void start() throws IOException {
+        String cmd = "";
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        while (!cmd.equals(CMD_QUIT)) { // terminates console if user input is "quit
+            System.out.print(CLI_LINE); // prints the console-like string
+
+            cmd = reder.readLine(); // takes input from user. user input should be started with
+            // "hello", "answer" or "quit"
+            String[] cmdArr = cmd.split(" ");
+
+            if (cmdArr[0].equals(CMD_HELLO)) { // exceutes when user input starts with "hello"
+                hello(cmdArr);
+            } else if (cmdArr[0].equals(CMD_ANSWER)) { // executes when user input starts with "answer"
+                answer(cmdArr);
+            }
+        }
+    }
+
+    // prints "Hello World!" on the screen if user input starts with "hello"
+    private void hello(String[] cmdArr) {
+        System.out.println("Hello World!");
+    }
+
+    // prints "42" on the screen if user input starts with "answer"
+    private void answer(String[] cmdArr) {
+        System.out.println("42");
+    }
+}
+/// 10:37
