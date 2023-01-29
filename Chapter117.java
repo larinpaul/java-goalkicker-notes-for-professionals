4d896a47946086ec18bdf11530b8bedec1e705df
Pavel Larin <www.larinpavel@mail.ru>
2023-01-29 11:28:35 +0500

    Finishing the Chapter 117 on my PC

--- Chapter117.java
+++ Chapter117.java
@@ -267,10 +267,200 @@ public class Chapter117 {
 
 
     /// Section 117.8: Indentation
+    // * Indentation level is four spaces.
+    // * Only space characters may be used for indentation. No tabs.
+    // * Empty lines must not be indented. (This is implied by the no trailing white space rule.)
+    // * case lines should be indented with four spaces, and statements within the case should bei ndented with
+    // another four spaces.
+    switch(var) {
+        case TWO:
+            setChoice("two");
+            break;
+        case Three:
+            setChoice("three");
+            breal;
+        default:
+            throw new IllegalArgumentException();
+    }
+
+    // Refer to Wrapping statements for guidelines on how to indent continuation lines.
 
 
+    /// Section 117.9: Literals
+    long l = 5432L;
+    int i = 0x123 + 0xABC;
+    byte b = 0b1010;
+    float f1 = 1 / 5432f;
+    float f2 = o.123e4f;
+    double d1 = 1 / 5432d; // or 1 / 5432.0
+    double d = 0x1.3p2;
+    // * long literals should use the upper case letter L suffix.
+    // * Hexadecimal literals should use upper case letters A-F.
+    // * All other numerical prefixes, infixes, and suffixes should use lowercase letters.
 
 
+    /// Section 117.10: Package declaration
+    package com.example.my.package;
+    // The package declaration should not be line wrapped, regardless of whether it exceeds the recommended
+    // maximum length of a line.
 
+
+    /// Section 117.11: Lambda Expressions
+    Runnable r = () -> System.out.println("Hello World");
+
+    Supplier<String> c = () -> "Hello World";
+    // Collection::contains is a simple unary method and its behavior is
+    // clear from the context. A method reference is preferred here.
+    appendFilter(goodStrings::contains);
+
+    // A lambda expression is easier to understand than just tempMap::put in this case
+    trackTemperature((time, temp) -> tempMap.put(time, temp));
+    // * Expression lambdas are preferred over single-line block lambdas.
+    // * Method references should generally be preferred over lambda expressions.
+    // * For bound instance method references, or methods with arity greater than one, a lambda expression may be
+    // easier to udnerstand and therefroe preferred. Especially if the behavior of the method is not clear from the
+    // context.
+    // * The parameter types should be omitted unless they improve readability.
+    // * If a lambda expression stretches over more than a few lines, consider creating a method.
+
+
+    /// Section 117.12: Java Source Files
+    // * All lines must be terminated with a line feed character (LF, ASCII value 10) and not for instance CR or CR+LF.
+    // * There may be no trailing white space at the end of a line.
+    // * The name of a source file must equal the name of the class it contains followed by the .java extension, even
+    // for files that only contain a package private class. This does not apply to files that do not contain any class
+    // declarations, such as package-info.java.
+
+
+    /// Section 117.13: Wrapping statements
+    // * Source code and comments should generally not exceed 80 characters per line and rarely if ever exceed 100
+    // characters per line, including indentation.
+
+    // * Source code and comments should generally not exceed 80 characters per line and rarely if ever exceed 100
+    // characters per line, including indentation.
+
+    // The character limit must be judged on a case by case basis. What really matters is the semantical "density"
+    // and readability of the line. Making lines gratuitously long makes them hard to read; similarly, making "heroic
+    // attemps" to fit them into 80 columns can also make them hard to read. The flexibilty outlined here aims to
+    // enable developers to avoid these extremes, not to maximize use of moinitor real-estate.
+    // * URLs or example commands should no=t be wrapped.
+
+    // Ok even though it might exceed max line width when indented.
+    Error e = isTypeParam
+            ? Errors.InvalidRepeatableAnnotationNotApplicable(targetContianerType, on)
+            : Errors.InvalidRepeatableAnnotationNotApplicabeInContext(targetContainerType);
+
+    // Wrapping preferable
+    String pretty = Stream.of(args)
+            .map(Argument::prettyPring)
+            .collectors(joining(", "));
+
+    // Too string interpretation of max line width. Readability suffers.
+    Error e = isTypeParam
+            ? Errors.InvalidReapeatableAnnotationNotApplicable(
+                    targetContainerType, on)
+            : Errors.InvalidRepeatableAnnotationNotApplicableInContext(
+                    targetContainerType);
+
+    // Should be wrapped even though it fits within the character limit
+    String pretty = Stream.of(args).map(Argument::prettyPrint).collectors(joining(", "));
+    // * Wrapping at a higher syntactical level is preferred over wrapping at a lower syntactical level.
+    // * There should be at most 1 statement per line.
+    // * A continuation line should be indented in one of the following four ways
+        // * Variant 1: With 8 extra spaces relative to the indentation of the previous line.
+        // * Variant 2: With 8 extra spaces relative to the starting column of the wrapped expression.
+        // * Variant 3: Aligned with previous sibling expression (as long as it is clear that it's a continuation line)
+        // * Variant 4: Aligned with previous method call in a chained expression.
+
+
+    /// Section 117.14: Wrapping Method Declarations
+    int someMethod(String aString,
+                   List<Integer> aList,
+                   Map<String, String> aMap,
+                   int anInt,
+                   long aLong,
+                   Set<Number> aSet,
+                   double aDouble) {
+        ...
+    }
+
+    int someMethod(String aString, List<Integer> aList,
+                   Map<String, String> aMap, int anInt, long aLong,
+                   doublew aDouble, long aLong) {
+        ...
+    }
+
+    int someMethod(String aString,
+                   List<Map<Integer, StringBuffer>> aListOfMaps,
+                   Map<String, String> aMap)
+            throws IllegalArgumentException {
+        ...
+    }
+
+    int someMethod(String aString, List<Integer> aList,
+                   Map<String, String> aMap, int anInt)
+                        throws IllegalArgumentException {
+        ...
+    }
+    // * Method declarations can be formated by listing the arguments vertically, or by a new line and +8 extra
+    // spaces
+    // * If a throws clause needs to be wrapped, put the line break in front of the throws clause and make sure it
+    // stands out form the argument list, either by indenting +8 relative to the function declaration, or +8 relative to
+    // the previous line.
+
+
+    /// Section 117.15: Wrapping Expressions
+    // * If a line approaches the maximum character limit, always consider breaking it doewn into multiple statements
+    // / expressions instead of wrapping the line.
+    // * Break before operators.
+    // * Break before the . in chained method calls.
+    popupMsg("Inbox notification: You have "
+        + newMsgs + " new messages");
+
+    // Don't! Looks like two argumnets
+    popupMsg("Inbox notification: You have " +
+            newMsgs + " new messages");
+
+
+    /// Section 117.16: Whitespace
+
+    // Vertical Whitespace
+    // * A single blank line should be used to separate...
+        // * Package declaration
+        // * Class declarations
+        // * Constructors
+        // * Methods
+        // * Static initializers
+        // * Instance initializers
+    // * ...and may be used to separate logical groups of
+        // * import statements
+        // * fields
+        // * statements
+    // * Multiple consecutive blank lines should only be used to separate groups of related members and not as the
+    // standard inter-member spacing.
+
+    // Horizontal Whitespace
+    // * A single space should be used...
+        // * To separate keywords from neighboring openinng or closing brackets and braces
+        // * Before and after all binary operators and operator like symbols such as arrows in lambda expressions
+        // and the colon in enhanced for loops (but not before the colon of a label)
+        // * After // that starts a comment
+        // * After commas separating arguments and semicolons separatingthe parts of a for loop.
+        // * After the closing parenthesis of a cast.
+    // * In variable declarations it is not recommended to align types and variables.
+
+    /// Section 117.17: Special Characters
+    // * Apart from LF the only allowed white space character is Space (ASCII value 32). Note that this implies that
+    // otehr white space characters (in, for isntance, stirng and character literals must be written in escaped from.
+    // * \', \", \\, \t, \b, \r, \f, and \n should be preferred over corresponding ocatal (e.g. \047) or Univocde (e.g.
+    // \u0027) escaped characters.
+    // * Should there be a need to go againstt the above rules for the sake of testing, the test should generate the
+    // required input programmatically.
+
+
+    /// Section 117.18: Variable Declarations
+    // * One variable perdeclaration (and at most one declaration per line)
+    // * Square brackets of arrays should be at the type (String[] args) and not on the variable (String args[]).
+    // * Declare a local variable right before it is first used, and initialize it as close to the declaration as possible.
 
 }
